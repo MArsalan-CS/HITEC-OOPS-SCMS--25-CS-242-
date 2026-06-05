2a188395d3e6b68daa1d4dbbd10580bff6a8a2eb
MArsalan-CS <malikarsalan2789@gmail.com>
2026-06-05 21:10:18 +0500

    Update overdueexception.h

---
+++ PERSON HIREARCHY/Library/overdueexception.h
@@ -0,0 +1,17 @@
+#ifndef OVERDUEEXCEPTION_H
+#define OVERDUEEXCEPTION_H
+
+#include <iostream>
+using namespace std;
+class Overdueexception
+{
+    private:
+        double fine;
+
+        public:
+        Overdueexception(double f)
+        {
+            fine = f;
+        }
+        
+}
